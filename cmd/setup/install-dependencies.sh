39ac3c733a8e256cdb2e7a0c47bc1c1ea975ca48
kellervater <kellervater@gmail.com>
2025-11-26 17:24:57 +0100

    fix(renovate): bosl2 updates

--- cmd/setup/install-dependencies.sh
+++ cmd/setup/install-dependencies.sh
@@ -13,7 +13,7 @@
 
 set -euo pipefail
 
-# renovate: datasource=github-commits depName=BelfrySCAD/BOSL2 versioning=loose
+# renovate: datasource=github-commits depName=BelfrySCAD/BOSL2 versioning=git
 BOSL2_VERSION="088d17ddd81d246fa1f0672a89a61c62958b7cee"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
